aa813126b6dc42cf9642a9fdddcd1469bc0410f4
anarmh <anarmh@code.edu.az>
2023-02-12 23:35:40 -0800

    update

--- Task1/assets/js/script.js
+++ Task1/assets/js/script.js
@@ -1,38 +1,42 @@
 "use strict";
 
-let user1 ={
+let user1={
+
     name:"Anar",
-    surname:"Huseynov",
-    age:36
-};
-let user2 ={
+    surname: "Huseynov",
+    age: 36
+}
+let user2={
+
     name:"Tunar",
-    surname:"Huseynli",
-    age:25
-};
-let user3 ={
+    surname: "Huseynli",
+    age: 26
+}
+let user3={
+
     name:"Elnar",
-    surname:"Huseynli",
-    age:15
-};
+    surname: "Huseynli",
+    age: 15
+}
+let user4={
 
-let users=[user1,user2,user3];
-
-
-function getUsersAgeByDate(users,calback){
-
-    const today = new Date();
-   
-    
-    
-    for (const item of users) {
-        if(calback(item)){
-            
-            return Math.abs(today.getFullYear - item.age);
-        }
-        
-    }
+    name:"Masum",
+    surname: "Huseynov",
+    age: 16
 }
 
+let users=[user1,user2,user3,user4];
 
-console.log(getUsersAgeByDate(users,m=>m.age>20));
+
+function getUserofData(users){
+    let date=new Date();
+    let year=date.getFullYear();
+
+    for (const item of users) {
+        let birthDate=year-item.age
+        console.log(`Name: ${item.name} Surname: ${item.surname} BirthDate: ${birthDate}`);
+    }
+
+}
+
+getUserofData(users.filter(m=>m.age>20));
